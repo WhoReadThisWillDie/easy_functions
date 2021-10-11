4b7f2931e988547fc261737d1ca337022c8a4257
WhoReadThisWillDie <78028560+WhoReadThisWillDie@users.noreply.github.com>
2021-10-11 11:31:23 +0300

    Add files via upload

--- square_functions.cpp
+++ square_functions.cpp
@@ -2,30 +2,41 @@
 
 int itc_skv(int num1)
 {
-    return itc_abs(num1 * num1);
+    if (num1 > 0)
+        return num1 * num1;
+    return -1;
 }
 
 int itc_spr(int num1, int num2)
 {
-    return itc_abs(num1 * num2);
+    if (num1 > 0 && num2 > 0)
+        return num1 * num2;
+    return -1;
 }
 
-double itc_str(double num1, double num2, double num3)
+int itc_str(int num1, int num2, int num3)
 {
-    double half;
-    double num4;
-
-    half = (num1 + num2 + num3) / 2;
+    if (num1 <= 0 || num2 <= 0 || num3 <= 0)
+        return -1;
+    if ((num1 + num2 < num3) || (num2 + num3 < num1) || (num1 + num3 < num2))
+        return -1;
+    int half, num4;
+    half = num1 + num2 + num3;
+    half = half / 2;
     num4 = half * (half - num1) * (half - num2) * (half - num3);
+    if (itc_sqrt(num4) <= 0)
+        return -1;
     return itc_sqrt(num4);
 }
 
 double itc_scir(int num1)
 {
     double res;
-
-    res = (num1 * num1) * 3.14;
-    return res;
+    if (num1 > 0) {
+        res = (num1 * num1) * 3.14;
+        return res;
+    }
+    return -1;
 }
 
 double itc_pow(int num1, int num2)
